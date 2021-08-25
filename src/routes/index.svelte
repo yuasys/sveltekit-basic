2347ff99f1d5b87d46835bccd8ee9428da9d604c
湯浅 信彦 <2534721+yuasys@users.noreply.github.com>
2021-08-26 03:17:06 +0900

    sveltekit tutorial 7

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,19 @@
-<h1>Homepage</h1>
-<p>これはトップページです</p>
+<script context="module">
+  export const load = async ({ fetch }) => {
+    const res = await fetch("/api/post")
+    const data = await res.json()
+    console.log(data)
 
+    return {
+      props: {
+        data,
+      }
+    }
+  }
+</script>
+<script>
+  export let data
+</script>
+
+{JSON.stringify(data)}
+<h1>Homepage</h1>
